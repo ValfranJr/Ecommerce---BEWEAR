8e5ad2096f11eb8f0aead0696cbdb0a5fca8ae0d
Valfran <valfran-goncalves@hotmail.com>
2025-08-06 10:44:21 -0300

    feat: add category products page

--- src/components/common/category-selector.tsx
+++ src/components/common/category-selector.tsx
@@ -1,5 +1,6 @@
 import { categoryTable } from "@/db/schema";
 import { Button } from "../ui/button";
+import Link from "next/link";
 
 interface CategorySelectorProps {
   categories: (typeof categoryTable.$inferSelect)[];
@@ -15,7 +16,7 @@ const CategorySelector = ({ categories }: CategorySelectorProps) => {
             variant="ghost"
             key={category.id}
           >
-            {category.name}
+            <Link href={`/category/${category.slug}`}>{category.name}</Link>
           </Button>
         ))}
       </div>
