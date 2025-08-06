2fa775968187889d9b84cbeb3f335651e76c6a11
Valfran <valfran-goncalves@hotmail.com>
2025-08-05 22:10:51 -0300

    feat: add product list and product item components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,40 @@
 import { Header } from "@/components/common/header";
-const Home = () => {
+import ProductList from "@/components/common/product-list";
+import { db } from "@/db";
+import Image from "next/image";
+const Home = async () => {
+  const products = await db.query.productTable.findMany({
+    with: {
+      variants: true,
+    },
+  });
   return (
     <>
       <Header />
+      <div className="space-y-6">
+        <div className="px-5">
+          <Image
+            src="/banner01.png"
+            alt="leve uma vida com estilo"
+            width={0}
+            height={0}
+            sizes="100vw"
+            className="h-auto w-full"
+          />
+        </div>
+        <ProductList title="Mais Vendidos" products={products} />
+
+        <div className="px-5">
+          <Image
+            src="/banner02.png"
+            alt="seja autêntico"
+            width={0}
+            height={0}
+            sizes="100vw"
+            className="h-auto w-full"
+          />
+        </div>
+      </div>
     </>
   );
 };
--- src/db/seed.ts
+++ src/db/seed.ts
@@ -1,5 +1,3 @@
-import crypto from "crypto";
-
 import { db } from ".";
 import { categoryTable, productTable, productVariantTable } from "./schema";
 
